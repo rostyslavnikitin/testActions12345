e48e4177f994fe58ab21fb2947dd96ee13e4e9bf
ross <rosstindev@gmail.com>
2021-03-24 04:46:27 +0200

    add env usage

--- test_actions/pages/_app.js
+++ test_actions/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+console.log("\n\n NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR:", process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR, "\n\n")
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
