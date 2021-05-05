36e17f186fb7da118410cf45460698d0a525a622
ross <rosstindev@gmail.com>
2021-05-05 15:10:06 +0300

    update actions

--- test_actions/pages/_app.js
+++ test_actions/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 
 console.log("\n\n NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR:", process.env.NEXT_PUBLIC_SENTRY_SERVER_ROOT_DIR, "\n\n")
+console.log("\n\n foo * bar:", process.env.NEXT_PUBLIC_BAR, "\n\n")
 console.log("\n\n foo * bar:", Number(process.env.NEXT_PUBLIC_FOO) *  Number(process.env.NEXT_PUBLIC_BAR), "\n\n")
 
 function MyApp({ Component, pageProps }) {
